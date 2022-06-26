c8d2519cb1372ebb6f0ed17aac8104fbaac0254e
Waqas Ahmad <waqaskanju@gmail.com>
2022-06-26 17:27:19 +0500

    change index

--- src/modules/get-meals.js
+++ src/modules/get-meals.js
@@ -1,5 +1,5 @@
 const getMealData = async () => {
-  const mealIdUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=beef';
+  const mealIdUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Biryani';
   const getData = await fetch(`${mealIdUrl}`);
   const receivedMeals = await getData.json();
   return receivedMeals;
--- src/modules/render-meals.js
+++ src/modules/render-meals.js
@@ -8,8 +8,14 @@ const showMeals = () => {
   const cardData = getMealData();
   cardData.then((value) => {
     if (value.meals) {
-      for (let i = 1; i <= 6; i += 1) {
-        const image = value.meals[i].strMealThumb;
+      for (let i = 0; i <= value.meals.length; i += 1) {
+        let image = '';
+        if (!value.meals[i].strMealThumb) {
+          image = 'https://www.seekpng.com/ipng/u2t4y3q8w7o0o0w7_404-error-not-found/';
+        } else {
+          image = value.meals[i].strMealThumb;
+        }
+
         const title = value.meals[i].strMeal;
         const id = value.meals[i].idMeal;
         const category = value.meals[i].strCategory;
